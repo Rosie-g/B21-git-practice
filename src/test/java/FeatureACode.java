24e06ba3f4cf029cd78ec6938f0e6e24e4e10c78
Rosie-g <ruz0408@gmail.com>
2021-01-26 11:23:19 -0800

    more work on feature A

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,5 +3,6 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Look what I added");
+        System.out.println("B21 is learning");
     }
 }
